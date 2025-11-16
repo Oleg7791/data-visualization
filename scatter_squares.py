483c8dda3edf3308b7da38ce1bf53ad8b8822450
oleg <o1egov@mail.ru>
2025-11-16 14:20:07 +0700

    автоматическое вычисление данных

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 
 x_values = list(range(1, 1000))
 y_values = [i ** 2 for i in x_values]
+
+
 plt.style.use('fast')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.tab20b, s=10)
 
 # назначение заголовка диаграммы и меток осей
 ax.set_title('Square Numbers', fontsize=24)
