bd4404e9707d4cdb4c692ce93cfedeaad39cf586
oleg <o1egov@mail.ru>
2025-11-16 19:18:26 +0700

    начальные и конечные точки

--- rw_visual.py
+++ rw_visual.py
@@ -11,9 +11,13 @@ while True:
 
     # нанесение точек на диаграмму
     plt.style.use('classic')
-    fig,ax = plt.subplots()
+    fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+               cmap=plt.cm.Blues, edgecolors='none', s=15)
+    # выделение первой и последней точек
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
 
     keep_running = input('Make another walk?(y/n)')
