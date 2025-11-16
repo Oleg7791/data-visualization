1d2ec62e160bae3ae54de17f7761b58b1c9e70b2
oleg <o1egov@mail.ru>
2025-11-16 14:02:36 +0700

    автоматическое вычисление данных

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,7 +4,7 @@ x_values = list(range(1, 1000))
 y_values = [i ** 2 for i in x_values]
 plt.style.use('fast')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 # назначение заголовка диаграммы и меток осей
 ax.set_title('Square Numbers', fontsize=24)
@@ -12,6 +12,6 @@ ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Square of Value', fontsize=14)
 
 # назначение размера шрифта делений на осях
-ax.tick_params(axis='both', which='major', labelsize=14)
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
