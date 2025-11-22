860a2b9ea498ee6c6f14f64c3255034ebdecc555
oleg <o1egov@mail.ru>
2025-11-22 10:11:26 +0700

    удаление осей

--- rw_visual.py
+++ rw_visual.py
@@ -17,7 +17,11 @@ while True:
                cmap=plt.cm.Blues, edgecolors='none', s=15)
     # выделение первой и последней точек
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1],
+               c='red', edgecolors='none', s=100)
+    #  удаление осей
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input('Make another walk?(y/n)')
